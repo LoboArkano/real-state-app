95dbf24aa4f9fc96b4946f2ea75d41fcfc3330bb
LoboArkano <pipiolo_mananes@hotmail.com>
2021-11-26 17:11:35 -0600

    Import fetchApi func

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
+import { baseUrl, fetchApi } from '../utils/fetchApi';
 
 const Banner = (props) => {
   const {purpose, title1, title2, imageUrl, desc1, desc2, linkName, buttonText} = props;
@@ -26,7 +27,9 @@ const Banner = (props) => {
   )
 };
 
-export default function Home() {
+export default function Home({propertiesForSale, propertiesForRent}) {
+  console.log(propertiesForSale, propertiesForRent);
+
   return (
     <Box>
       <Banner
@@ -51,4 +54,16 @@ export default function Home() {
       />
     </Box>
   )
+}
+
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
+  const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`);
+
+  return {
+    props: {
+      propertiesForSale: propertyForSale?.hits,
+      propertiesForRent: propertyForRent?.hits,
+    },
+  };
 }
