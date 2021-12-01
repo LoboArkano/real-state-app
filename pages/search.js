b743891dd5571be200015dfac41f400a8aa30da2
LoboArkano <pipiolo_mananes@hotmail.com>
2021-12-01 15:19:26 -0600

    Add template for properties

--- pages/search.js
+++ pages/search.js
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
-import Image from "@chakra-ui/image";
+import Image from "next/image";
 import { Flex, Box, Text, Icon } from '@chakra-ui/react';
 import { BsFilter } from 'react-icons/bs';
 import SearchFilters from '../components/SearchFilters';
+import Property from '../components/Property';
+import noresult from '../assets/images/noresult.svg';
 
 const Search = () => {
   const [searchFilters, setSearchFilters] = useState(false);
   const router = useRouter();
 
   return (
-    <Box>
+    <Box minH="90vh">
       <Flex
         cursor="pointer"
         bg="gray.100"
@@ -27,6 +29,21 @@ const Search = () => {
         <Icon paddingLeft="2" w="7" as={BsFilter} />
       </Flex>
       {searchFilters && <SearchFilters />}
+      <Text fontSize="2xl" p="4" fontWeight="bold">
+        Properties {router.query.purpose}
+      </Text>
+      <Flex flexWrap="wrap">
+        {[].map((property) => <Property property={property} key={property.id} />)}
+      </Flex>
+      {[].length === 0 && (
+        <Flex
+          justifyContent="center" alignItems="center" flexDirection="column"
+          marginTop="5" marginBottom="5"
+        >
+          <Image src={noresult} alt="no result" />
+          <Text fontSize="2xl" marginTop="3">No Results Found</Text>
+        </Flex>
+      )}
     </Box>
   )
 }
